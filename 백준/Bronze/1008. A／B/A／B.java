23c21bad1e6691ab383b7f49cecff1f1277eb65f
MiSeongKim <kimms000311@naver.com>
2025-01-02 19:05:40 +0900

    [Bronze V] Title: A/B, Time: 176 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a;
+        int b;
+
+        Scanner sc = new Scanner(System.in);
+        // System.out.println("A입력");
+        a = sc.nextInt();
+        // System.out.println("B입력");7
+        b = sc.nextInt();
+        // if (a < 0 || a > 10 || b < 0 || b > 10) {
+        // throw new NumberFormatException();
+        // }
+        double result = (double)a / b;
+        System.out.println(result);
+
+    }
+}
