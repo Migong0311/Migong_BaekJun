aa2320fe3021e2cac1e3f1aa7a093fc48848745a
MiSeongKim <kimms000311@naver.com>
2025-01-02 19:00:57 +0900

    [Bronze V] Title: 사칙연산, Time: 176 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a;
+        int b;
+
+        Scanner sc = new Scanner(System.in);
+//        System.out.println("A입력");
+        a = sc.nextInt();
+  //      System.out.println("B입력");
+        b = sc.nextInt();
+        // if (a < 0 || a > 10 || b < 0 || b > 10) {
+        // throw new NumberFormatException();
+        // }
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
